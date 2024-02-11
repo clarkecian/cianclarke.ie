c43acfa4b93d79c1a2d91b9dd6464a3a29ddb0aa
Cian Clarke <88947580+clarkecian@users.noreply.github.com>
2024-02-11 22:31:49 +0000

    colour update

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
 # [cianclarke.ie](https://www.cianclarke.ie)
 
-Welcome to my home on the web. My website is a simple static site using HTML and CSS. I'm sure there is plenty of room for improvement so I hope to update it regularly.
-
-## Contributing
-
-Pull requests are welcome. For major changes, please open an issue first
-to discuss what you would like to change.
-
-Please make sure to update tests as appropriate.
-
-## Acknowledgments
-
-Fonts provided by [Google Fonts](https://fonts.google.com).
-
-Social Icons provided by [Font Awesome](https://fontawesome.com).
+Files to cianclarke.ie
--- script.js
+++ script.js
@@ -10,5 +10,20 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
   var currentYear = new Date().getFullYear();
   var currentYearElement = document.getElementById('current-year');
-  currentYearElement.textContent = currentYear;
+  currentYearElement.textContent = convertToRoman(currentYear);
 });
+
+function convertToRoman(num) {
+  var romanNumerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  var arabicNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var result = '';
+
+  for (var i = 0; i < arabicNumbers.length; i++) {
+    while (num >= arabicNumbers[i]) {
+      result += romanNumerals[i];
+      num -= arabicNumbers[i];
+    }
+  }
+
+  return result;
+}
